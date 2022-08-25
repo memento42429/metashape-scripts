3633ddfb2fe8ad71492daa11100d9824f79cf4b7
memento42429 <memento42429@gmail.com>
2022-08-25 13:05:36 +0900

    add sys.argv for suffix

--- src/contrib/export_models.py
+++ src/contrib/export_models.py
@@ -1,4 +1,6 @@
+from mimetypes import suffix_map
 import Metashape
+import sys
 
 """
 Metashape disable model Script (v 1.0)
@@ -6,7 +8,7 @@ Kent Mori, Feb 2022
 
 Usage:
 Workflow -> Batch Process -> Add -> Run script
-This scrip export whole models of chunks. 
+This scrip export whole models of chunks. When use system arguments[1], you can add suffix at the export file names.
 """
 
 compatible_major_version = "1.8"
@@ -17,12 +19,20 @@ if found_major_version != compatible_major_version:
 doc = Metashape.app.document
 chunks = doc.chunks
 
+print(len(sys.argv))
+if len(sys.argv) == 1: # default is no suffix.
+    suffix = ""
+else: # suffix is made from sys.argv 
+    suffix = "_" + str(sys.argv[1])
+
+
+
 for chunk in chunks:
     if chunk.enabled is True:
         if chunk.model is None:
             continue
         else:
-            chunk.exportModel(path = "/".join(doc.path.split("/")[:-1]) + "/" + chunk.label + "_models/fromMS/" + chunk.label + ".obj",
+            chunk.exportModel(path = "/".join(doc.path.split("/")[:-1]) + "/" + chunk.label + "_models/fromMS/" + chunk.label + suffix + ".obj",
             texture_format=Metashape.ImageFormat.ImageFormatPNG)
             #ImageFormatJPEG
 # export each quarity models of each chunks
